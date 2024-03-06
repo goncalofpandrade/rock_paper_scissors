1f47de526fddd0fb9519811f7d5fa00644bf4850
Goncalo Andrade <127350474+goncalofpandrade@users.noreply.github.com>
2024-03-06 18:15:15 +0000

    When a computer selects 3 random numbers.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //user inputs a string -> rock, paper or scissors to lower case
 let playerSelection = prompt("Choose one rock, paper or scissors: ", "").toLowerCase();
+
+
+function getComputerChoice() {
+    //computer selects 3 random numbers
+    let randomComputerSelection = Math.floor(Math.random() * 3);
+}
